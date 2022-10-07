a4b49b840475d86843666e1042f4728b2a2fb381
Madaraka02 <victormadaraka@gmail.com>
2022-10-08 00:48:24 +0300

    added signals for profile models

--- advocates/models.py
+++ advocates/models.py
@@ -1,5 +1,35 @@
 from django.db import models
+from companies.models import CompanyProfile
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from django.db.models.signals import post_save 
+from django.dispatch import receiver
+
 
 # Create your models here.
 class Profile(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    profile_pic = models.URLField(blank=True)
+    short_bio = models.CharField(max_length=300,blank=True)
+    long_bio = models.TextField(blank=True)
+    advocate_years_exp = models.PositiveIntegerField(default=1)
+    company = models.ForeignKey(CompanyProfile, on_delete=models.SET_NULL, null=True)
+    youtube = models.URLField(null=True,blank=False)
+    twitter = models.URLField(null=True,blank=False)
+    github = models.URLField(null=True,blank=False)
+
+    def __str__(self):
+        return self.user.name
+
+    @receiver(post_save, sender=User)
+    def create_advocate_profile(sender, instance, created, **kwargs):
+        if created:
+            if instance.is_advocate == True:
+            # instance.is_advocate
+                Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_advocate_profile(sender, instance, **kwargs):
+        if instance.is_advocate == True:
+            instance.profile.save()
--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from django.db.models.signals import post_save 
+from django.dispatch import receiver
+
 
 # Create your models here.
+
+class CompanyProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    logo = models.URLField(blank=True)
+    summary = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.name
+
+    @receiver(post_save, sender=User)
+    def create_company_profile(sender, instance, created, **kwargs):
+        if created:
+            if instance.is_company == True:
+                CompanyProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_company_profile(sender, instance, **kwargs):
+        if instance.is_company == True:
+            instance.companyprofile.save()
